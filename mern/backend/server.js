77b6d000d373b845502f4a6e96d1ef939933101a
Lekhan2004 <luckylekhan25@gmail.com>
2025-11-28 09:42:21 +0530

    added Kubernetets + promethesus required code

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -2,12 +2,25 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 
+const client = require('prom-client');
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+
+// Create a Registry to register metrics
+const register = new client.Registry();
+
+// 1. Collect Default Metrics (CPU, Memory, GC, Event Loop)
+client.collectDefaultMetrics({ register });
+
+// 2. Expose the /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 // start the Express server
 app.listen(PORT, () => {
--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-service:5050', // This MUST be the K8s service name
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
